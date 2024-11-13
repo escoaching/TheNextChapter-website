2915adbdf524dfe3641b1de987608d23f0f5adda
AONIQQ <info@aoniqq.com>
2024-11-13 16:49:30 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import autoCert from "anchor-pki/auto-cert/integrations/next";
 
 const withAutoCert = autoCert({
   enabledEnv: "development",
+  domains: ['emotionalsobrietycoaching.com'], 
 });
 
 export default withAutoCert({});
