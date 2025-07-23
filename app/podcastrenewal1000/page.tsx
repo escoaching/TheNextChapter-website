3369e2dd1cc59de59e5e54f5388b9251346308fc
escoaching <colleen@emotionalsobrietycoaching.com>
2025-07-23 17:03:56 -0400

    Update page.tsx

--- app/podcastrenewal1000/page.tsx
+++ app/podcastrenewal1000/page.tsx
@@ -567,7 +567,7 @@ export default function PodcastRenewalPage() {
 
                 <div className="mt-12">
                   <p className="text-lg text-[#46474c] mb-4 font-montserrat">If you&apos;d like to purchase with HSA/FSA click this button:</p>
-                  <form name="PrePage" method = "post" action = "https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx"> <input type = "hidden" name = "LinkId" value ="115c472a-52db-46f3-b7cf-e8b36820888d" /> <input type = "image" src ="//content.authorize.net/images/buy-now-gold.gif" /> </form>
+                  <form name="PrePage" method = "post" action = "https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx"> <input type = "hidden" name = "LinkId" value ="dd6348de-25eb-4aa6-8fb9-ec432d63e2ef" /> <input type = "image" src ="//content.authorize.net/images/buy-now-gold.gif" /> </form>
                 </div>
                 <div className="mt-8">
                   <p className="text-lg text-[#46474c] mb-4 font-montserrat">To checkout with Affirm, use the button below:</p>
